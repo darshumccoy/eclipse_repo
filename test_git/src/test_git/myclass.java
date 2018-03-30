937258acb1f22ec31281b513790ee0d9dc1a6c60
darshumccoy <darshinisheku@gmail.com>
2018-03-30 13:25:31 +0530

    added a class

---
+++ test_git/src/test_git/myclass.java
@@ -0,0 +1,14 @@
+package test_git;
+
+public class myclass {
+	
+	public static void main(String[] args)
+	{
+		
+		System.out.println("first add this to git");
+		System.out.println("then add this to jenkins");
+		
+		
+	}
+
+}
